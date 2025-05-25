3603a5592173fce46bf48ccb3952ec81205c838f
Kirill <kiw197204@gmail.com>
2025-05-26 00:20:49 +0400

    feat: depth limit

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { schema } from './types/gqlTypes.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -15,7 +16,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-    async handler(req) {
+    async handler(req, reply) {
+      const document = parse(req.body.query);
+      const errors = validate(schema, document, [depthLimit(5)]);
+      if (errors.length > 0) {
+        return reply.code(400).send({
+          errors: errors.map((err) => ({ message: err.message })),
+        });
+      }
       return graphql({
         schema,
         source: req.body.query,
